957bf455622f256a50d5a92b210a4c3bb1475c3e
Yaroslav <yarik93v@gmail.com>
2026-09-09 13:39:34 -0700

    863. All Nodes Distance K in Binary Tree

--- src/863. All Nodes Distance K in Binary Tree.kt
+++ src/863. All Nodes Distance K in Binary Tree.kt
@@ -26,10 +26,57 @@ fun main() {
 }
 
 fun distanceK(root: TreeNode?, target: TreeNode?, k: Int): List<Int> {
-    return emptyList()
+    val map = mutableMapOf<TreeNode, MutableList<TreeNode>>()
+    val queue = ArrayDeque<TreeNode>()
+    if (root == null || target == null) {
+        return listOf()
+    }
+    if (k == 0) {
+        return listOf(target.`val`)
+    }
+    queue.add(root)
+    while (queue.isNotEmpty()) {
+        val node = queue.removeFirst()
+        node.left?.let { left ->
+            map.getOrPut(node) { mutableListOf() }.add(left)
+            map.getOrPut(left) { mutableListOf() }.add(node)
+            queue.add(left)
+        }
+        node.right?.let { right ->
+            map.getOrPut(node) { mutableListOf() }.add(right)
+            map.getOrPut(right) { mutableListOf() }.add(node)
+            queue.add(right)
+        }
+    }
+
+    val visited = mutableSetOf<TreeNode>()
+    queue.clear()
+    queue.add(target)
+    val nodesList = mutableListOf<Int>()
+    var distance = 0
+    while (queue.isNotEmpty() && distance != k) {
+        repeat(queue.size) {
+            val node = queue.removeFirst()
+            visited.add(node)
+            val neighbors = map[node]
+            if (!neighbors.isNullOrEmpty()) {
+                queue.addAll(neighbors.filter { it !in visited })
+            }
+        }
+        distance++
+        if (distance == k) {
+            return queue.toList().map { it.`val` }
+        }
+    }
+
+    return nodesList
 }
 
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+
+    override fun toString(): String {
+        return "$`val`"
+    }
 }
